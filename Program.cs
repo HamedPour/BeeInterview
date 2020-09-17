1fffbedd45c355c776ac654a7ae5ee4b6a9962d6
HamedPour <hamed_k.pour@hotmail.com>
2020-09-17 15:02:18 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
