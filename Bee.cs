f183141debb6a637a9d4346f0eb2de6ab3487206
HamedPour <hamed_k.pour@hotmail.com>
2020-09-18 12:56:57 +0100

    percetage health reduction

--- Bee.cs
+++ Bee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,29 +10,34 @@ namespace BeeInterview
     abstract class Bee
     {
         private float Health { get; set; }
-        public bool IsAlive { get; set; }
+        public bool Dead { get; set; }
         public virtual float DamageLimit { get; set; }
         public Bee()
         {
             Health = 100;
-            IsAlive = true;
+            Dead = false;
         }
 
         public void Damage(float amount)
         {
-            if (!IsAlive) return;
+            float percetage = amount / 100;
+            // if the bee is dead don't bother with anything else and just return.
+            if (Dead) return;
+            // if the argument is not in range of 0 100, tell them and return.
             if (amount > 100 || amount < 0)
             {
                 Console.WriteLine("Damage must be between 0 and 100.");
                 return;
             }
+            // if the bee has reached it's damage limit set Dead to true, return.
             if (Health <= DamageLimit)
             {
-                IsAlive = false;
+                Dead = true;
                 Console.WriteLine("The Bee is Dead!");
                 return;
             }
-            Health -= amount;
+            // if you've gotten this fare apply the damage amount to bee's health.
+            Health *= percetage;
         }
 
         public float getHealth()
--- Program.cs
+++ Program.cs
@@ -13,11 +13,18 @@ namespace BeeInterview
 
             Queen q = new Queen();
 
+            Console.WriteLine(q.getHealth());
             q.Damage(30);
             Console.WriteLine(q.getHealth());
-            Console.WriteLine(q.IsAlive);
             q.Damage(10);
-            Console.WriteLine(q.IsAlive);
+            Console.WriteLine(q.getHealth());
+            q.Damage(40);
+            Console.WriteLine(q.getHealth());
+            q.Damage(60);
+            Console.WriteLine(q.getHealth());
+            q.Damage(60);
+            Console.WriteLine(q.getHealth());
+
 
             Console.ReadLine();
         }
